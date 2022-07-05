aed3e680a51cd0cabe217b3f5adaabec441dfabf
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-07-05 19:26:01 +0000

    [Update]admin item_show

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,7 +3,6 @@ class Admin::ItemsController < ApplicationController
     @items = Item.page(params[:page])
   end
 
-
   def new
     @item = Item.new
     @genres = Genre.all
@@ -21,6 +20,13 @@ class Admin::ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    @genres = Genre.all
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   }
   namespace :admin do
     resources :genres, only:[:index, :create, :edit, :update]
-    resources :items, only:[:index, :new, :create, :show, :edit, :update]
+    resources :items, only:[:index, :new, :show, :edit, :create, :update]
   end
 
 
