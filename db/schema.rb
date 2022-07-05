75eabba2d0ad2dd3598808a9b31bcd525e33c0c6
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-07-05 16:51:27 +0000

    [Add]order_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_05_164906) do
+ActiveRecord::Schema.define(version: 2022_07_05_165015) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -73,6 +73,19 @@ ActiveRecord::Schema.define(version: 2022_07_05_164906) do
     t.text "introduction", null: false
     t.integer "price", null: false
     t.boolean "is_active", default: true, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "name", null: false
+    t.integer "postage", null: false
+    t.integer "price", null: false
+    t.integer "payment_method", default: 0, null: false
+    t.integer "order_status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
