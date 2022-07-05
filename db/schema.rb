46c22c3e0338d258641ad28d9c8d966e5523e997
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-07-05 16:48:44 +0000

    [Add]genre_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_05_164519) do
+ActiveRecord::Schema.define(version: 2022_07_05_164807) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -51,6 +51,12 @@ ActiveRecord::Schema.define(version: 2022_07_05_164519) do
     t.boolean "is_active", default: false, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "items", force: :cascade do |t|
