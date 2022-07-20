f438ab2e208d01530e48dca4aa479d3a6c60d0c6
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-07-20 18:18:23 +0000

    [Update]public order_confirm_layouts

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,14 +5,31 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    render :confirm
+    @cart_items = @customer.cart_items.all
+    @total = 0
+    @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.first_name + current_customer.last_name
   end
 
   def complete
+  end
+
+  def create
+
   end
 
   def index
   end
 
   def show
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 end
