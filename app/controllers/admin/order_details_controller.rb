70169be11bae7ccc1cad162fcb04bcdd5a238169
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-07-26 03:58:44 +0000

    [Update]admin making_status update

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,9 +2,12 @@ class Admin::OrderDetailsController < ApplicationController
 
   def update
     order_detail = OrderDetail.find(params[:id])
+    order_details = OrderDetail.where(order_id: order_detail.order.id).pluck(:making_status)
     order_detail.update(order_detail_params)
     if order_detail.making_status == "making"
       order_detail.order.update(order_status: 2)
+    elsif order_details.all?{ |s| s == "complete" }
+      order_detail.order.update(order_status: 3)
     end
     redirect_to admin_order_path(id: order_detail.order)
   end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -52,7 +52,7 @@ class Public::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @order_details = OrderDetail.where(order_id: params[:id])
+    @order_details = @order.order_details
   end
 
   private
