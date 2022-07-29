ae9b2579eb4df7bfcfa4c44e8ac8f1ed4d1a84ef
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-07-29 07:57:18 +0000

    [Update]public item_sidebar layouts

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,10 +2,12 @@ class Public::ItemsController < ApplicationController
   def index
     @item_count = Item.all
     @items = Item.page(params[:page]).per(8)
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
   end
 end
